aedc2bd0890466cdd113cbe53b557f675acafdf5
Cassio Camargo <143041157+Cassiokmargo@users.noreply.github.com>
2025-04-30 16:32:41 -0300

    Update api mockada e README

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Movie } from "../types";
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://my-json-server.typicode.com/Cassiokmargo/luz-e-cena-api';
 
 export const getMovies = async (): Promise<Movie[]> => {
    const response = await axios.get<Movie[]>(`${BASE_URL}/movies`)
